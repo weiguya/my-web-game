b9af78ea7de7139e7db0c3432abb63d3489fb865
weiguya <weiguya@gmail.com>
2024-11-12 11:30:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-// Firebase Configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCCZoEDlhBkixk09r7pMTK405cTcLCNwDM",
-  authDomain: "simple-chat-room-c51e7.firebaseapp.com",
-  databaseURL: "https://simple-chat-room-c51e7-default-rtdb.firebaseio.com",
-  projectId: "simple-chat-room-c51e7",
-  storageBucket: "simple-chat-room-c51e7.firebasestorage.app",
-  messagingSenderId: "1079302562257",
-  appId: "1:1079302562257:web:46acb19989a7529dd840f0",
-  measurementId: "G-32LY5EPZTV"
-};
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
-// Test Write to Database
-const testRef = db.ref('test-node');
-testRef.set({
-  message: "Test message from Firebase!",
+const db = getDatabase(app);
+const testRef = ref(db, 'test-node');
+set(testRef, {
+  message: "Hello from Firebase!",
   sender: "Tester"
 }).then(() => {
   console.log("Data written successfully!");
