ea2bf639f2c0f58e436281d3c0d8a904db38cd29
weiguya <weiguya@gmail.com>
2024-11-12 10:53:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -85,3 +85,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+const chatRef = db.ref('chat-test');
+chatRef.set({
+  message: "Hello Firebase!",
+  sender: "TestUser"
+});
